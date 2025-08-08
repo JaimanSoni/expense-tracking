787ccf82c5af3c22ec14b3344bebf84ad1f0d2cc
Jaiman Soni <118343973+JaimanSoni@users.noreply.github.com>
2025-08-08 23:23:58 +0530

    Update constants.ts

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,5 +1,5 @@
-export const BASE_API_URL = "http://localhost:3000";
-// const BASE_API_URL = "https://expense-tracking-flax.vercel.app";
+// export const BASE_API_URL = "http://localhost:3000";
+export const BASE_API_URL = "https://expense-tracking-flax.vercel.app";
 
 export const API_URLS = {
   EXPENSE: {
